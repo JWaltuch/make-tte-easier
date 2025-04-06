f60e84bff3cd634d7aa971a7514c93b7d1cfd2b4
JWaltuch <jenna.waltuch@gmail.com>
2025-04-05 22:17:53 -0400

    sortable grid works

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 'use client'
 
 import React, { useEffect, useState } from 'react';
-import Paper from '@mui/material/Paper';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import TableSortLabel from '@mui/material/TableSortLabel';
+import { Box } from '@mui/material';
 
 const ORIGINS_2025_ID = "8D0356F0-D38B-11EF-9091-1D8264B1C7F0";
 const CLOCKTOWER_2025_ID = "32D6B730-365B-11EF-B58A-DCC620F8A28C";
@@ -32,7 +31,7 @@ export default function Page() {
   const [rowData, setRowData] = useState([]);
   const [rows, setRows] = useState<BasicRowType[]>([]);
   const [cols, setCols] = useState<GridColDef<(typeof rows)[number]>[]>(COL_NAMES.map((name) => {
-    return {field: name, headerName: name, width: 90}
+    return {field: name, headerName: name, width: 150}
   }));
 
   useEffect(() => {
@@ -40,14 +39,10 @@ export default function Page() {
   }, [])
 
   useEffect(() => {
-    if (rows.length > 0) {
-      const colNames = ['name', 'description', 'startdaypart_name', 'type_id', 'date_created', 'space_name', 'room_name'];
-      const columns: GridColDef<(typeof rows)[number]>[] = colNames.map((name) => {
-        return {field: name, headerName: name, width: 90}
-    });
-      setCols(columns);
-      setRows(rowData.filter((row) => {
+    if (rowData.length > 0) {
+      setRows(rowData.map((row) => {
         return {
+          id: row.id,
           name: row.name,
           description: row.description,
           startdaypart_name: row.startdaypart_name,
@@ -60,54 +55,17 @@ export default function Page() {
     }
   }, [rowData])
 
+  console.log(rows)
   return (
 
-    <Paper sx={{ height: 400, width: '100%' }}>
-    <DataGrid
-      rows={rows}
-      columns={cols}
-      initialState={{ pagination: { } }}
-      pageSizeOptions={[100, 1000]}
-      sx={{ border: 0 }}
-    />
-</Paper>
-//   <TableContainer component={Paper}>
-//   <Table sx={{ minWidth: 650, maxWidth: 800 }} aria-label="simple table">
-//     <TableHead>
-//       <TableRow>
-//         {cols.map((col) => (
-//           <TableCell>{col}</TableCell>
-//         ))}
-//       </TableRow>
-//     </TableHead>
-//     <TableBody>
-//       {rows.map((row) => {
-//         let counter = 0;
-//         return (
-//           <TableRow
-//             key={row.id}
-//           >
-//             <TableCell key={1} component="th" scope="row">{row.name}</TableCell>
-//             <TableCell key={2} component="th" scope="row">{row.description}</TableCell>
-//             <TableCell key={3} component="th" scope="row">{row.startdaypart_name}</TableCell>
-//             <TableCell key={4} component="th" scope="row">{row.type_id}</TableCell>
-//             <TableCell key={5} component="th" scope="row">{row.date_created}</TableCell>
-//             <TableCell key={6} component="th" scope="row">{row.room_name}</TableCell>
-//             <TableCell key={7} component="th" scope="row">{row.space_name
-//               }</TableCell>
-//             {/* {Object.values(row).map((cell: string) => {
-//               console.log(cell)
-//               return (
-//                 <TableCell key={counter++} component="th" scope="row">
-//                 {typeof(cell) !== "object" ? String(cell).substring(0, 50) : "tbd"}
-//               </TableCell>
-//               )}
-//             )} */}
-//           </TableRow>
-//         )}
-//       )}
-//     </TableBody>
-//   </Table>
-// </TableContainer>
+    <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={cols}
+        initialState={{ pagination: { } }}
+        pageSizeOptions={[100, 1000]}
+        sx={{ border: 0 }}
+      />
+  </Box>
 );
 }
